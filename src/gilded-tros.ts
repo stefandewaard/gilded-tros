2e96aa6302b19233b84589348eec6ec8cf0cd6bc
Stefan de Waard <stefandewaard@live.nl>
2024-02-29 12:37:40 +0100

    business rules

--- src/gilded-tros.ts
+++ src/gilded-tros.ts
@@ -7,15 +7,35 @@ export class GildedTros {
 		for (let item of this.items) {
 			switch (item.name) {
 				case "Good Wine":
+					item = this.updateGoodWine(item);
 					break;
 				case "Backstage passes for Re:Factor":
 				case "Backstage passes for HAXX":
+					item = this.updateBackstagePasses(item);
+					break;
 					break;
 				case "B-DAWG Keychain":
 					break;
 				default:
+					item = this.updateOne(item);
 					break;
 			}
 		}
+	}
+
+	private updateOne(item: Item): Item {
+		const { sellIn, quality } = item;
+		item.sellIn = sellIn - 1;
+		item.quality =
+			quality > 0 ? (sellIn < 0 ? quality - 2 : quality - 1) : quality;
+		return item;
+	}
+
+	private updateGoodWine(item: Item): Item {
+		return item;
+	}
+
+	private updateBackstagePasses(item: Item): Item {
+		return item;
 	}
 }
